0108a0ed07ae73d0e1dc23094ba4c54defe3ed78
hanfiden <fidendu2@gmail.com>
2021-10-03 01:57:50 +0200

    completed services page

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,12 +72,28 @@ tabs.forEach(tab =>{
   })
 })
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClosses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, x) =>{
+    modalBtn.addEventListener('click', () =>{
+      modal(x)
+    })
+})
+
+modalClosses.forEach((modalClose) =>{
+  modalClose.addEventListener('click', () =>{
+    modalViews.forEach((modalView) =>{
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
