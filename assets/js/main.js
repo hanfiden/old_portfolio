63e278baedd19fe4ec385efaffac6e4a5c8effb0
hanfiden <fidendu2@gmail.com>
2021-10-06 14:29:54 +0200

    completion portfolio page

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,15 +96,15 @@ modalClosses.forEach((modalClose) =>{
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper(".portfolio__container", {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  mousewheel: true,
-  keyboard: true,
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
